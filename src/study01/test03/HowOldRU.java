052b1ae85341efa49afde8a26d6d13a1bd13d1d6
Administrator <Administrator@koitt03a-PC>
2019-09-27 15:20:21 +0900

    "How old are you?" exercise

--- src/study01/test03/HowOldRU.java
+++ src/study01/test03/HowOldRU.java
@@ -8,7 +8,21 @@ public class HowOldRU {
 		System.out.println("How old are you?");
 		String age = scan.nextLine();
 		System.out.println("I 'm " + age + " years old.");
-		int num = Integer.parseInt(age);
-		System.out.println(age);
+		try {
+			int num = Integer.parseInt(age);
+			if(num > 0 && num <= 10) {
+				System.out.println("You're a child.");
+			} else if(num <=20) {
+				System.out.println("You're a teenager.");
+			} else if(num <= 40) {
+				System.out.println("You're an adult.");
+			} else if(num <= 0) {
+				System.out.println("Please enter a valid age.");
+			} else {
+				System.out.println("You're very old!!");
+			}
+		} catch(Exception e) {
+			System.out.println("Please enter in numbers.");
+		}
 	}
 }
