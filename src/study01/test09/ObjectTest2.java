1a7bee04df27cc3d1fd1def529d43afd413b28ba
Administrator <Administrator@koitt03a-PC>
2019-10-07 20:45:05 +0900

    2019-10-07 ObjectTest2 update

--- src/study01/test09/ObjectTest2.java
+++ src/study01/test09/ObjectTest2.java
@@ -13,15 +13,18 @@ public class ObjectTest2 {
 	public static void main(String[] args) {
 		ObjectTest2 ot2 = new ObjectTest2();
 		ot2.num1 = 10;
-		System.out.println(ot2.num1);
+		System.out.println("num1 (new object) : " + ot2.num1);
 		
 		// Because num2 is "static", we don't need to indicate the memory location like "num1"
 		// num2 = ObjectTest.num2
-		System.out.println(num2);
+		System.out.println("num2 : " + num2);
 		num2 = 3;
-		System.out.println(ObjectTest2.num2);
+		System.out.println("num2 (updating the value) : " + ObjectTest2.num2);
+		ObjectTest2 ot3 = new ObjectTest2();
+		System.out.println("num1 (new object) : " +ot3.num1);
+		System.out.println("num2 (new object) : " +ot3.num2);
 
 		// Need to indicate where it's located. (Because it's outside of the current class)
-		System.out.println(ObjectTest3.num2);
+		System.out.println("num2 (Different static int) : " + ObjectTest3.num2);
 	}
 }
