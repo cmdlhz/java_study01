86a55c58239ac8ac382aa34b1edcdd540d343e4e
Administrator <Administrator@SAMSUNG>
2019-10-09 13:18:36 +0900

    2019-10-09 test08/Jindo3 edit

--- src/study01/test08/Jindo3.java
+++ src/study01/test08/Jindo3.java
@@ -1,7 +1,9 @@
 package study01.test08;
 
 class Dog3{
-	
+	public void greet() {
+		System.out.println("hello");
+	}
 }
 
 class Puddle3{
@@ -12,10 +14,7 @@ public class Jindo3 extends Dog3 {
 	public static void main(String[] args) {
 		// Jindo3 inherits Dog3.
 		Dog3 d3 = new Jindo3();
-
-		//********* ERROR
-//		Dog d3 = new Dog3();
-		
+	
 		//********* ERROR
 //		Dog d3 = new Puddle3();
 	}
