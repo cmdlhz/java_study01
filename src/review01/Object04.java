808abf1c6fa39bfc95bed95332ac76370aff71ab
Administrator <Administrator@koitt03a-PC>
2019-10-15 17:34:47 +0900

    2019-10-15 Problem - solved

--- src/review01/Object04.java
+++ src/review01/Object04.java
@@ -1,14 +1,14 @@
 package review01;
 
-public class Object {
+public class Object04 {
 	String str1 = "";
 	String str2 = "";
 	String str3 = "te";
 	String str4 = "te";
 	
 	public static void main(String[] args) {
-		Object ot = new Object();
-		Object ot2 = new Object();
+		Object04 ot = new Object04();
+		Object04 ot2 = new Object04();
 		
 		System.out.println(ot);	
 		System.out.println(ot2);			
